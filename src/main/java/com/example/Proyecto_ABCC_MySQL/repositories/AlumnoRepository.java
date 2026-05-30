79f72d5903d33f1c78d84e6de5e433b73586cf30
ED A <eduardo.br@tecjerez.edu.mx>
2026-05-29 22:38:51 -0600

    Repositorio para usuarios

--- src/main/java/com/example/Proyecto_ABCC_MySQL/repositories/AlumnoRepository.java
+++ src/main/java/com/example/Proyecto_ABCC_MySQL/repositories/AlumnoRepository.java
@@ -1,7 +1,6 @@
 package com.example.Proyecto_ABCC_MySQL.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.Proyecto_ABCC_MySQL.models.AlumnoModel;
 
 public interface AlumnoRepository extends JpaRepository<AlumnoModel, Long>{}
---
+++ src/main/java/com/example/Proyecto_ABCC_MySQL/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.Proyecto_ABCC_MySQL.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.Proyecto_ABCC_MySQL.models.UsuarioModel;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+    Optional<UsuarioModel> findByUsername(String username);
+}
